0a40d75d4be2ee27bc481aa9736e678f14a16fd9
lovejatps <huxxiao@163.com>
2016-09-07 20:18:58 +0800

    Create git.sh

---
+++ git.sh
@@ -0,0 +1,11 @@
+yum install curl-devel expat-devel gettext-devel openssl-devel zlib-devel
+yum install  gcc perl-ExtUtils-MakeMaker
+wget https://github.com/git/git/archive/v2.2.1.tar.gz
+tar zxvf v2.2.1.tar.gz
+cd git-2.2.1
+make configure
+./configure --prefix=/usr/local/git --with-iconv=/usr/local/libiconv
+make all doc
+make install install-doc install-html
+echo "export PATH=$PATH:/usr/local/git/bin" >> /etc/bashrc
+source /etc/bashrc
